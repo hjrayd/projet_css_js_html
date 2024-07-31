293213fdd3bb3b28f1d198fd79439f5fdd44fd08
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-07-31 19:12:34 +0200

    project 2 finished

--- project_2/script.js
+++ project_2/script.js
@@ -9,10 +9,14 @@ updateText();
 
 function updateText(){
     characterIndex++
-    containerEl.innerHTML = `<h1>I am a ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+    containerEl.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "I"  ? "an" : "a"}${careers[careerIndex].slice(0,characterIndex)}</h1>`;
     if(characterIndex === careers[careerIndex].length){
         careerIndex++
         characterIndex = 0
+    }
+
+    if(careerIndex === careers.length) {
+        careerIndex = 0;
     }
     setTimeout(updateText, 400);
 }
